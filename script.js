19e0c8bb356108a1838d18528c540728af85be96
Valdirene Elaine <valdirene.elaine@gmail.com>
2020-05-02 10:40:13 -0400

    ajustando a mensagens dos tipos de triângulos

--- script.js
+++ script.js
@@ -4,18 +4,31 @@ function classificar() {
     let lado3 = document.getElementById("lado3").value
 
     if ((lado1 == "") || (lado2 == "") || (lado3 == "")) {
+        document.querySelector(".resultado").style.visibility = "hidden"
         alert("Preencha todos os campos!")
     } else
     if ((lado1 == lado2) && (lado2 == lado3)) {
-        document.querySelector(".resultado").innerHTML = 'Triângulo Equilátero'
+        document.querySelector(".resultado p").innerHTML = 'Triângulo Equilátero'
+        document.getElementById("imagem").src = "img/equilatero.svg"
+        document.querySelector(".resultado").style.visibility = "visible"
+
 
     } else
     if ((lado1 == lado2) && (lado2 != lado3)) {
-        document.querySelector(".resultado").innerHTML = 'Triângulo Isósceles'
+        document.querySelector(".resultado p").innerHTML = 'Triângulo Isósceles'
+        document.getElementById("imagem").src = "img/isoceles.svg"
+        document.querySelector(".resultado").style.visibility = "visible"
 
     } else
     if ((lado1 != lado2) && (lado2 != lado3) && (lado1 != lado3)) {
-        document.querySelector(".resultado").innerHTML = 'Triângulo Escaleno'
+        document.querySelector(".resultado p").innerHTML = 'Triângulo Escaleno'
+        document.getElementById("imagem").src = "img/escaleno.svg"
+        document.querySelector(".resultado").style.visibility = "visible"
+
+    }
+
+    function limpar() {
+        document.querySelector(".resultado").style.visibility = "hidden"
 
     }
 
