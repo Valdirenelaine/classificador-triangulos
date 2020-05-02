e62d1a71d767eefe0edbe3ec49e443f972116b3b
Valdirene Elaine <valdirene.elaine@gmail.com>
2020-05-02 09:36:19 -0400

    adicionando a codificação dos triângulos

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ function classificar() {
     let lado1 = document.getElementById("lado1").value
     let lado2 = document.getElementById("lado2").value
     let lado3 = document.getElementById("lado3").value
-    let divreslutado = document.getElementById("resultado")
 
     if ((lado1 == "") || (lado2 == "") || (lado3 == "")) {
         alert("Preencha todos os campos!")
     } else
-    if ((lado1 == lado2) && (lado2 == lado3) && (lado3 == lado1)) {
-        divreslutado.innerHTML = 'Trinângulo Equilátero'
-        divreslutado.style.backgroundColor = "ligthgreen"
+    if ((lado1 == lado2) && (lado2 == lado3)) {
+        document.querySelector(".resultado").innerHTML = 'Triângulo Equilátero'
+
+    } else
+    if ((lado1 == lado2) && (lado2 != lado3)) {
+        document.querySelector(".resultado").innerHTML = 'Triângulo Isósceles'
+
+    } else
+    if ((lado1 != lado2) && (lado2 != lado3) && (lado1 != lado3)) {
+        document.querySelector(".resultado").innerHTML = 'Triângulo Escaleno'
 
     }
 
